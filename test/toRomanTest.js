109110f306b25b46c1c133473cd4eb574c0434c4
Pedro <pedro.valencia@thomascookonline.com>
2014-08-19 14:48:49 +0100

    Implementation of kata

--- test/toRomanTest.js
+++ test/toRomanTest.js
@@ -9,6 +9,36 @@ describe("Arabic to Roman", function(){
 
 	it("Number 1 should be I", function(){
 		assert.equal("I", toRoman.toRoman(1));
-	})
+	});
+
+	it("Number 5 should be V", function(){
+		assert.equal("V", toRoman.toRoman(5));
+	});
+
+	it("Number 10 should be X", function(){
+		assert.equal("X", toRoman.toRoman(10));
+	});
+
+	it("Number 50 should be L", function(){
+		assert.equal("L", toRoman.toRoman(50));
+	});
+
+	it("Number 100 should be C", function(){
+		assert.equal("C", toRoman.toRoman(100));
+	});
+
+	it("Number 500 should be D", function(){
+		assert.equal("D", toRoman.toRoman(500));
+	});
+
+	it("Number 1000 should be M", function(){
+		assert.equal("M", toRoman.toRoman(1000));
+	});
+
+	it("Number 1944 should be MCMXLIV", function(){
+		assert.equal("MCMXLIV", toRoman.toRoman(1944));
+	});
+
+
 
 });
--- toRoman.js
+++ toRoman.js
@@ -1,3 +1,36 @@
-module.exports.toRoman = function(arabic) {
-	return "I";
+var toRoman ={};
+toRoman.toRoman = function conversorFromArabicToRoman(arabic) {	
+	
+	if(arabic >= 1000) {
+		return"M"+conversorFromArabicToRoman(arabic - 1000);
+	} else if(arabic >= 900) {
+		return "CM"+conversorFromArabicToRoman(arabic - 900);
+	} else if(arabic >= 500) {
+		return"D"+conversorFromArabicToRoman(arabic - 500);
+	} else if(arabic >= 400) {
+		return"CD"+conversorFromArabicToRoman(arabic - 400);
+	} else if(arabic >= 100) {
+		return "C"+conversorFromArabicToRoman(arabic - 100);
+	} else if(arabic >= 90) {
+		return"XC"+conversorFromArabicToRoman(arabic - 90);
+	} else if(arabic >= 50) {
+		return "L"+conversorFromArabicToRoman(arabic - 50);
+	} else if(arabic >= 40) {
+		return"XL"+conversorFromArabicToRoman(arabic - 40);
+	} else if(arabic >= 10) {
+		return "X"+conversorFromArabicToRoman(arabic - 10);
+	} else if(arabic >= 9) {
+		return"IX"+conversorFromArabicToRoman(arabic - 9);
+	} else if(arabic >= 5) {
+		return "V"+conversorFromArabicToRoman(arabic - 5);
+	} else if(arabic >= 4) {
+		return"IV"+conversorFromArabicToRoman(arabic - 4);
+	} else if(arabic >=1) {
+		return "I"+ conversorFromArabicToRoman(arabic - 1);
+	} else {
+		return "";
+	}
+	
 } 
+
+module.exports = toRoman;
